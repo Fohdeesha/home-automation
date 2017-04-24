ae822eb8e754ca14ffe2488fadb35a6086435e1a
BuildTools <fohdeesha@gmail.com>
2017-04-24 04:11:05 -0400

    Arduino Temp Addition

--- Arduino/PLC_Arduino/PLC_Arduino.ino
+++ Arduino/PLC_Arduino/PLC_Arduino.ino
@@ -3,8 +3,11 @@
 #include <PubSubClient.h>
 #include <Wire.h>
 #include <Adafruit_ADS1015.h>
+#include "Adafruit_MCP9808.h"
+#include <MemoryFree.h>
 
 Adafruit_ADS1115 ads;
+Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
 
 byte mac[] = { 0x90, 0xA2, 0xDA, 0x0F, 0x5E, 0x69 };
 IPAddress ip(192, 168, 1, 21);
@@ -101,6 +104,7 @@ void setup()
   digitalWrite(4, HIGH);
   ads.setGain(GAIN_TWOTHIRDS);
   ads.begin();
+  tempsensor.begin();
 
   client.setServer(server, 1883);
   client.setCallback(callback);
@@ -118,9 +122,9 @@ void loop()
 
   delay(50);
 
-   if (millis() > ts + 3000) {   
+   //Call ADC/data parsing loop every 2s
+   if (millis() > ts + 2000) {   
        ts = millis();
-       //Call ADC loop every 3s
        loopADC();
    } 
 }
@@ -141,37 +145,47 @@ void loopADC()
   PSU12v = (adc3 + 16) / 1067.542;
   PSU5v = (adc4 + 16) / 1064.542;
   LED24v = AnalogIn1 / 40.840;
+  int bytes = freeMemory();
+
+  float c = tempsensor.readTempC();
+  float temp = c * 9.0 / 5.0 + 32;
   
   Serial.print("24v Supply #1: "); Serial.println(PSU24v1);
   Serial.print("24v Supply #2: "); Serial.println(PSU24v2);
   Serial.print("12v Rail: "); Serial.println(PSU12v);
   Serial.print("5v Rail: "); Serial.println(PSU5v);
   Serial.print("LED 24v Supply: "); Serial.println(LED24v);
+  Serial.print("Temperature: "); Serial.println(temp);
+  Serial.print("Free Memory Bytes: "); Serial.println(bytes);
+
   Serial.println(" ");
   
-    char Power1[5];
+    char Power1[10];
   dtostrf(PSU24v1,4,2,Power1);
-
   client.publish("/read/arduino_LED/PSU1",Power1);
 
-    char Power2[5];
+    char Power2[10];
   dtostrf(PSU24v2,4,2,Power2);
-
   client.publish("/read/arduino_LED/PSU2",Power2);
 
-    char Power3[5];
+    char Power3[10];
   dtostrf(PSU12v,4,2,Power3);
-
   client.publish("/read/arduino_LED/PSU3",Power3);
 
-    char Power4[5];
+    char Power4[10];
   dtostrf(PSU5v,4,2,Power4);
-
   client.publish("/read/arduino_LED/PSU4",Power4);
 
-    char Power5[5];
+    char Power5[10];
   dtostrf(LED24v,4,2,Power5);
-
   client.publish("/read/arduino_LED/PSU5",Power5);
-  
+
+    char tempchar[10];
+  dtostrf(temp,4,2,tempchar);
+  client.publish("/read/arduino_LED/temp",tempchar);
+
+
+    char memorychar[10];
+  itoa(bytes,memorychar,10);
+  client.publish("/read/arduino_LED/memory",memorychar);
 }
